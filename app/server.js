1ee33264a150396e4115919d186c0402cce86fdc
Hasan Moosavi <h.mousavi910@gmail.com>
2025-02-09 22:26:04 +0330

    Added Morgan pkg

--- app/server.js
+++ app/server.js
@@ -1,8 +1,9 @@
 import { fileURLToPath } from "url";
-import { connect } from "mongoose";
 
 import allRoutes from "./router/router.js";
+import mongoose from "mongoose";
 import express from "express";
+import morgan from "morgan";
 import dotenv from "dotenv";
 import path from "path";
 import http from "http";
@@ -24,6 +25,7 @@ export class Application {
   }
   async configApplication() {
     const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    this.#app.use(morgan("dev"));
     this.#app.use(express.json());
     this.#app.use(express.urlencoded({ extended: true }));
     this.#app.use(express.static(path.join(__dirname, "..", "public")));
@@ -35,8 +37,19 @@ export class Application {
   }
   async connectToMongoDB() {
     try {
-      await connect(this.#MONGO_URI);
+      await mongoose.connect(this.#MONGO_URI);
       console.log("Connected to DB");
+      mongoose.connection.on("connected", () => {
+        console.log("Mongoose connected to DB");
+      });
+      mongoose.connection.on("disconnected", () => {
+        console.log("Mongoose connection is disconnected");
+      });
+      process.on("SIGINT", async () => {
+        await mongoose.connection.close();
+        console.log("Mongoose connection closed due to app termination");
+        process.exit(0);
+      });
     } catch (error) {
       console.log(`Failed to Connect DB: ${error}`);
     }
